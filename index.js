b0652064611734ad2027eb99730d89aef61235d6
Farzana Mohsin <fmohsin@usertesting.com>
2024-04-25 20:37:07 -0400

    add GET api for /art/:id

--- index.js
+++ index.js
@@ -40,6 +40,13 @@ async function run() {
       res.send(result);
     });
 
+    app.get("/art/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await artCollection.findOne(query);
+      res.send(result);
+    });
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log(
