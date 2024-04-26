19d01b47042657e4c7a67f120e26739ab67cda84
Farzana Mohsin <fmohsin@usertesting.com>
2024-04-26 16:11:00 -0400

    fix connection to mongoDB and now successfully connected on vercel

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+require("dotenv").config();
+
 const express = require("express");
 const cors = require("cors");
 const app = express();
@@ -8,14 +10,14 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-const uri = "mongodb://localhost:27017";
-// const uri = "mongodb+srv://<username>:<password>@cluster81657.uygasmd.mongodb.net/?retryWrites=true&w=majority&appName=Cluster81657";
+// const uri = "mongodb://localhost:27017";
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster81657.uygasmd.mongodb.net/?retryWrites=true&w=majority&appName=Cluster81657`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
   serverApi: {
     version: ServerApiVersion.v1,
-    strict: true,
+    strict: false,
     deprecationErrors: true,
   },
 });
@@ -117,6 +119,7 @@ async function run() {
     // await client.close();
   }
 }
+
 run().catch(console.dir);
 
 app.get("/", (req, res) => {
